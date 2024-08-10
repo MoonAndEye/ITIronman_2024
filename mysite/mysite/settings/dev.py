f44c0a74db599259b136e2e9b6c1991b528e421b
MarvinLin <marvin_lin@cmoney.com.tw>
2024-08-10 12:54:56 +0800

    wagtail start mysite

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-u5y#fvme@iw!6w#s7oa=m!r#p@$qv3152_r)*ry@qi^j_!f!9f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
