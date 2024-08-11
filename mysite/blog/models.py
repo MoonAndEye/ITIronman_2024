ca365ae89a423ba3823e585278b8ee7740482c3d
MarvinLin <atimis19@gmail.com>
2024-08-11 19:17:06 +0800

    feat: 完成 tag system feat: tag button 過場 tag list

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,27 @@
-# New imports added for forms and ParentalManyToManyField, and MultiFieldPanel
 from django import forms
 from django.db import models
 
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
+
+# New imports added for ClusterTaggableManager, TaggedItemBase
+# 要將 ClusterTaggableManager, TaggedItemBase 加進來
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
 from wagtail.models import Page, Orderable
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.search import index
 from wagtail.snippets.models import register_snippet
+
+
+# 加上 BlogPageTag 系統
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 
 
 class BlogIndexPage(Page):
@@ -29,21 +43,22 @@ class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    authors = ParentalManyToManyField('blog.Author', blank=True)
+    
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
         index.SearchField('body'),
     ]
     
-    # Add this:
-    # 加上 authors
-    authors = ParentalManyToManyField('blog.Author', blank=True)
-
-    # content_panels 修改成這樣，把 authors 加進去
     content_panels = Page.content_panels + [
         MultiFieldPanel([
             FieldPanel('date'),
             FieldPanel('authors', widget=forms.CheckboxSelectMultiple),
+
+            # Add this:
+            FieldPanel('tags'),
         ], heading="Blog information"),
         FieldPanel('intro'),
         FieldPanel('body'),
@@ -82,3 +97,17 @@ class Author(models.Model):
 
     class Meta:
         verbose_name_plural = 'Authors'
+        
+
+class BlogTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        blogpages = BlogPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['blogpages'] = blogpages
+        return context
