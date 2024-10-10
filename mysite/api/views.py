1de2ab3123fd3089b346145f6d51d99ab5944866
MarvinLin <atimis19@gmail.com>
2024-10-10 23:29:44 +0800

    lint: black

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -6,10 +6,12 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def hello_world(request):
     return Response({"message": "Hello, World!"})
+
 
 @api_view(["POST"])
 def login_view(request):
@@ -20,10 +22,13 @@ def login_view(request):
 
     if user is not None:
         refresh = RefreshToken.for_user(user)
-        return JsonResponse({
-            "message": "Login successful!",
-            "access_token": str(refresh.access_token),
-            "refresh_token": str(refresh)
-        }, status=200)
+        return JsonResponse(
+            {
+                "message": "Login successful!",
+                "access_token": str(refresh.access_token),
+                "refresh_token": str(refresh),
+            },
+            status=200,
+        )
     else:
         return JsonResponse({"message": "Invalid credentials."}, status=401)
--- mysite/settings.py
+++ mysite/settings.py
@@ -1,12 +1,12 @@
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
 }
 
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
